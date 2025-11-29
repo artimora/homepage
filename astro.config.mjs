e21041c15719df87b3d211bbef10e4cd27e63f9c
copperdevs <github@copperdevs.com>
2025-11-29 14:30:54 -0600

    jampacking

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-
-import sitemap from "@astrojs/sitemap";
-
-
-import react from "@astrojs/react";
-
 
 import cloudflare from "@astrojs/cloudflare";
-
-
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import studiocmsUi from "@studiocms/ui";
-
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://artimora.dev",
   integrations: [sitemap(), react(), studiocmsUi()],
   adapter: cloudflare(),
+  trailingSlash: "ignore",
+  output: "static",
+  compressHTML: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 });
---
+++ jampack.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+export default {
+  html: {
+    sort_attributes: true,
+  },
+  js: {
+    compressor: "swc",
+  },
+  css: {
+    inline_critical_css: true,
+  },
+  compress: true,
+};
