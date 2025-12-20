558b54f140f0216710fe562dc053e1540713ab0b
copperdevs <github@copperdevs.com>
2025-12-19 23:03:29 -0600

    simplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,18 @@
 
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
-import studiocmsUi from "@studiocms/ui";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://artimora.dev",
-  integrations: [sitemap(), studiocmsUi()],
-  adapter: cloudflare(),
-  trailingSlash: "ignore",
-  output: "static",
-  compressHTML: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+	site: "https://artimora.dev",
+	integrations: [sitemap()],
+	adapter: cloudflare(),
+	trailingSlash: "ignore",
+	output: "static",
+	compressHTML: true,
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
 });
